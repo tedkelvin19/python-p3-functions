5a7d5c6c9c8df81ea2c492080efcf7d99c86cba6
Ted-Kelvin-Mutwiri <tedkelvin19@gmail.com>
2023-11-23 13:40:07 +0300

    Completed lab

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,21 @@
 
 def greet_programmer():
     pass
-
+    print("Hello, programmer!")
+greet_programmer()
 def greet(name):
     pass
-
+    print(f"Hello, {name}!")
+greet("Khalifa")
 def greet_with_default(name="programmer"):
     pass
-
-def add(num1, num2):
-    pass
-
-def halve(number):
-    pass
+    print(f"Hello, {name}!")
+greet_with_default()
+def add(num1: int, num2: int) -> int:
+    result = num1 + num2
+    return result
+add(45, 55)    
+def halve(number: float)-> float:
+    result = number / 2
+    return result
+halve(99.0)    
